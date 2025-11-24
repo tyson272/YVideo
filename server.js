95598b63007d69b7870df4130c3c242d629f0bfa
tyson272 <kakarlapavankumar07@gmail.com>
2025-11-24 14:13:53 +0530

    Use Render PORT env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Serve static files and parse forms
 app.use(express.static('public'));
